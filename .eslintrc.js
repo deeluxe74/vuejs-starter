17151a84d2c3b7fd249aa4d8e31d9954e74871d8
deeluxe74 <rollet.raphael@gmail.com>
2022-08-04 14:02:29 +0200

    base setup for vuejs project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: [2, 'single', 'avoid-escape'],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+      }
+    ]
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import vuetify from '@/plugins/vuetify'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import '@/assets/css/tailwind.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  vuetify,
+  render: (h) => h(App)
+}).$mount('#app')
